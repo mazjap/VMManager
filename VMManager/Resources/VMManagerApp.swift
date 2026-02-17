be7d6b62f9ef20f32660dc6be0f59d34fffbc975
Jordan Christensen <jordan.c4922@gmail.com>
2026-02-17 14:04:25 -0700

    Rename vmLifecycleService to lifecycleService and add configValidator to CreateNewVMViewModel Add more tasks to todo

--- VMManager/Resources/VMManagerApp.swift
+++ VMManager/Resources/VMManagerApp.swift
@@ -4,6 +4,8 @@ import SwiftData
 // Get rid of fatalErrors and swap them with runtime user-interactive errors (graceful failure) (also fix try?s and try!s)
 // Handle relevant TODOs
 // Seperation of concerns/better project structure
+// Shared clipboard between VM and host
+// Drag and drop files between VM and host
 
 @main
 struct VMManagerApp: App {
